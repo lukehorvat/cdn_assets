fafc439ed25e5f0fd99f7368311ae23c670cc333
Luke Horvat <lukehorvat@gmail.com>
2013-07-07 01:03:43 +1000

    Implemented crude function to conditionally include a JS or CSS asset on a page based on the asset name provided to it.

--- lib/cdn_assets/view_helpers.rb
+++ lib/cdn_assets/view_helpers.rb
@@ -1,7 +1,16 @@
 module CdnAssets
   module ViewHelpers
-    def hello(name)
-      content_tag :h1, 'Hello, %s!' % name
+    JQUERY_VERSION = Jquery::Rails::JQUERY_VERSION
+    ASSET_URLS = {
+      :jquery => "//ajax.googleapis.com/ajax/libs/jquery/#{JQUERY_VERSION}/jquery.min.js",
+      :bootstrap => "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css",
+      :bootstrap_no_icons => "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css",
+      :font_awesome => "//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css"
+    }
+
+    def cdn_asset(asset)
+      url = ASSET_URLS[asset]
+      url.end_with?(".js") ? javascript_include_tag(url) : stylesheet_link_tag(url)
     end
   end
 end
