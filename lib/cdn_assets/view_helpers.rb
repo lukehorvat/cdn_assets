d6b725254394ac429d07effb63b4fc83775328cd
Luke Horvat <lukehorvat@gmail.com>
2013-07-08 03:04:45 +1000

    Ordered asset urls alphabetically.

--- lib/cdn_assets/view_helpers.rb
+++ lib/cdn_assets/view_helpers.rb
@@ -1,28 +1,28 @@
 module CdnAssets
   module ViewHelpers
     ASSET_CDN_URLS = {
-      jquery: {
-        google: '//ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js',
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js',
-        microsoft: '//ajax.aspnetcdn.com/ajax/jQuery/jquery-%s.min.js'
-      },
-      bootstrap_js: {
-        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/js/bootstrap.min.js',
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/js/bootstrap.min.js'
-      },
       bootstrap: {
         cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/css/bootstrap.min.css'
       },
+      bootstrap_js: {
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/js/bootstrap.min.js',
+        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/js/bootstrap.min.js'
+      },
       bootstrap_responsive: {
-        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/css/bootstrap-combined.no-icons.min.css',
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/css/bootstrap-responsive.css'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/css/bootstrap-responsive.css',
+        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/css/bootstrap-combined.no-icons.min.css'
       },
       bootstrap_responsive_icons: {
         netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/css/bootstrap-combined.min.css'
       },
       font_awesome: {
-        netdna: '//netdna.bootstrapcdn.com/font-awesome/%s/css/font-awesome.min.css',
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/font-awesome/%s/css/font-awesome.min.css'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/font-awesome/%s/css/font-awesome.min.css',
+        netdna: '//netdna.bootstrapcdn.com/font-awesome/%s/css/font-awesome.min.css'
+      },
+      jquery: {
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js',
+        google: '//ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js',
+        microsoft: '//ajax.aspnetcdn.com/ajax/jQuery/jquery-%s.min.js'
       }
     }
 
