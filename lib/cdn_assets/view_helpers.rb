9d6b253ab8fc9546781f2b6f075f76ec94d9ad34
Luke Horvat <lukehorvat@gmail.com>
2013-07-09 22:44:02 +1000

    Switched to using gsub for version number injection.

--- lib/cdn_assets/view_helpers.rb
+++ lib/cdn_assets/view_helpers.rb
@@ -2,30 +2,30 @@ module CdnAssets
   module ViewHelpers
     ASSET_CDN_URLS = {
       bootstrap: {
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/css/bootstrap.min.css',
-        jsdelivr: '//cdn.jsdelivr.net/bootstrap/%s/css/bootstrap.min.css'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/{v}/css/bootstrap.min.css',
+        jsdelivr: '//cdn.jsdelivr.net/bootstrap/{v}/css/bootstrap.min.css'
       },
       bootstrap_js: {
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/js/bootstrap.min.js',
-        jsdelivr: '//cdn.jsdelivr.net/bootstrap/%s/js/bootstrap.min.js',
-        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/js/bootstrap.min.js'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/{v}/js/bootstrap.min.js',
+        jsdelivr: '//cdn.jsdelivr.net/bootstrap/{v}/js/bootstrap.min.js',
+        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/{v}/js/bootstrap.min.js'
       },
       bootstrap_responsive: {
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/%s/css/bootstrap-responsive.min.css',
-        jsdelivr: '//cdn.jsdelivr.net/bootstrap/%s/css/bootstrap-responsive.min.css',
-        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/css/bootstrap-combined.no-icons.min.css'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/{v}/css/bootstrap-responsive.min.css',
+        jsdelivr: '//cdn.jsdelivr.net/bootstrap/{v}/css/bootstrap-responsive.min.css',
+        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/{v}/css/bootstrap-combined.no-icons.min.css'
       },
       bootstrap_responsive_icons: {
-        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/%s/css/bootstrap-combined.min.css'
+        netdna: '//netdna.bootstrapcdn.com/twitter-bootstrap/{v}/css/bootstrap-combined.min.css'
       },
       font_awesome: {
-        jsdelivr: '//cdn.jsdelivr.net/fontawesome/%s/css/font-awesome.min.css',
-        netdna: '//netdna.bootstrapcdn.com/font-awesome/%s/css/font-awesome.min.css'
+        jsdelivr: '//cdn.jsdelivr.net/fontawesome/{v}/css/font-awesome.min.css',
+        netdna: '//netdna.bootstrapcdn.com/font-awesome/{v}/css/font-awesome.min.css'
       },
       jquery: {
-        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js',
-        google: '//ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js',
-        microsoft: '//ajax.aspnetcdn.com/ajax/jQuery/jquery-%s.min.js'
+        cdnjs: '//cdnjs.cloudflare.com/ajax/libs/jquery/{v}/jquery.min.js',
+        google: '//ajax.googleapis.com/ajax/libs/jquery/{v}/jquery.min.js',
+        microsoft: '//ajax.aspnetcdn.com/ajax/jQuery/jquery-{v}.min.js'
       }
     }
 
@@ -46,7 +46,7 @@ module CdnAssets
       url = cdn_urls[cdn.to_sym]
       raise ArgumentError, 'Invalid CDN name specified' if url.nil?
 
-      url = url % version.to_s
+      url = url.gsub '{v}', version.to_s
       if url.end_with? '.js'
         javascript_include_tag url, options
       elsif url.end_with? '.css'
